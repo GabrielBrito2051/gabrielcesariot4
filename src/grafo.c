0ba077a37f865814b2cec6b317bf2138152f73ac
Gabriel Cesario <gabriel.cesario@uel.br>
2026-06-18 15:26:32 -0300

    implementacao do algoritmo de dijkstra para menor distancia e menor tempo

--- src/grafo.c
+++ src/grafo.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <float.h>
 #include "../include/grafo.h"
 #include "../include/hashtable.h"
+#include "../include/fila-prioridade.h"
 
 typedef struct Aresta{
     char nome[48], ldir[24], lesq[24], destino[48];
@@ -105,16 +107,129 @@ void inserir_aresta(Grafo g, char* origem, char* destino, char* ldir, char*lesq,
     nova->prox = var->v[idOrigem].inicio;
     var->v[idOrigem].inicio = nova;
 }
-/*
-Grafo busca_menor_caminho(Grafo g){
 
+void busca_menor_distancia(Grafo g, char* idOrigem, char* idDestino){
+    if(g==NULL) return;
+    grafo* var = (grafo*) g;
+
+    int origem = obter_indice(var, idOrigem);
+    int destino = obter_indice(var,idDestino);
+
+    if(origem==-1 || destino==-1){
+        printf("Erro ao buscar o id de oigem ou id de destino");
+        return;
+    }
+
+    int n = var->qntdAtual;
+    float* distancia = malloc(n*sizeof(float));
+    int* antecessor = malloc(n*sizeof(int));
+    int* visitado = calloc(n*sizeof(int));
+
+    for(int i=0;i<n;i++){
+        distancia[i] = FLT_MAX;
+        antecessor[i] = -1;
+    }
+    distancia[origem] = 0.0;
+
+    Fila f = criar_fila(n*4);
+    inserir_fila(f, origem, 0.0);
+
+    while(!fila_vazia(f)){
+        int u = remove_minimo(f);
+        if(visitado[u]) continue;
+        visitado[u] = 1;
+        if(u==destino) break;
+
+        aresta* rua = var->v[u].inicio;
+        while(rua!=NULL){
+            int v = rua->destino;
+            float novaDistancia = distancia[u] + rua->cmp;
+
+            if(!visitado[v] && novaDistancia < distancia[v]){
+                distancia[v] = novaDistancia;
+                antecessor[v] = u;
+                inserir_fila(f, v, novaDistancia);
+            }
+            rua->prox;
+        }
+    }
+
+    if(distancia[destino]==FLT_MAX){
+        printf("Nao foi encontrada uma rota entre %s e %s\n", idOrigem, idDestino);
+    }else{
+        printf("Menor distancia:\nOrigem: %s\t Destino:%s\nDistancia total:%.2f km\n", idOrigem, idDestino, distancia[destino]);
+    }
+
+    destruir_fila(f);
+    free(distancia);
+    free(antecessor);
+    free(visitado);
 }
 
 
-Grafo busca_menor_tempo(Grafo g){
+Grafo busca_menor_tempo(Grafo g, char* idOrigem, char* idDestino){
+    if(g==NULL) return;
+    grafo* var = (grafo*) g;
 
+    int origem = obter_indice(var, idOrigem);
+    int destino = obter_indice(var, idDestino);
+
+    if(origem == -1 || destino == -1){
+        printf("Erro ao o id de origem ou de destino");
+        return;
+    }
+
+    int n = var->qntdAtual;
+
+    float* tempoAcumulado = malloc(n*sizeof(float));
+    int* antecessor = malloc(n*sizeof(int));
+    int* visitado = calloc(n*sizeof(int));
+
+    for(int i=0;i<n;i++){
+        tempoAcumulado[i] = FLT_MAX;
+        antecessor[i] = -1;
+    }
+
+    tempoAcumulado[origem] = 0.0;
+    Fila f = criar_fila(n*4);
+    inserir_fila(f, origem,0.0);
+
+    while(!fila_vazia(f)){
+        int u = remove_minimo(f);
+        if(visitado[u]) continue;
+        visitado[u] = 1;
+
+        if(u==destino) break;
+
+        aresta* rua = var->v[u].inicio;
+        while(rua!=NULL){
+            int v = rua->destino;
+
+            float tempo = rua->cmp / rua->vm;
+            float tempoTotal = tempoAcumulado[u] + tempo;
+
+            if(!visitado[v] && tempoTotal<tempoAcumulado[v]){
+                tempoAcumulado[v] = tempoTotal;
+                antecessor[v] = u;
+                inserir_fila(f, v, tempoTotal);
+            }
+            rua = rua->prox;
+        }
+    }
+    if(tempoAcumulado[destino] = FLT_MAX){
+        printf("Nao foi encontrado uma rota de %s ate %s", idOrigem, idDestino);
+    }else{
+        printf("Rota mais rapida:\n");
+        printf("Origem:%s\tDestino:%s\n", idOrigem, idDestino);
+        float minutos = tempoAcumulado[destino] * 60.0;
+        printf("Tempo total:%.1f minutos", minutos);
+    }
+
+    destruir_fila(f);
+    free(antecessor);
+    free(tempoAcumulado);
+    free(visitado);
 }
-*/
 
 void destruir_grafo(Grafo g){
     if(g==NULL) return;
